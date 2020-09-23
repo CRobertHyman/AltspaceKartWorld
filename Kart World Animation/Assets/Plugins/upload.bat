479251af922cf0897cc217d30398c7a6f678be3d
CRobertHyman <CRobertHyman@users.noreply.github.com>
2020-09-23 09:42:34 -0400

    Slight revision

--- Kart World Animation/Assets/Plugins/upload.bat
+++ Kart World Animation/Assets/Plugins/upload.bat
@@ -1,2 +1,2 @@
 echo off
-curl -v -b cookie -X PUT -F "space_template[zip]=@E:\Unity Class\Class 2\SecondAltspaceWorld\New Unity Project\template.zip" -F "space_template[game_engine_version]=20192" https://account.altvr.com/api/space_templates/1546740628468007234.json
+curl -v -b cookie -X PUT -F "space_template[zip]=@E:\Unity Class\Class 2\KartWorld\AltspaceKartWorld\Kart World Animation\template.zip" -F "space_template[game_engine_version]=20192" https://account.altvr.com/api/space_templates/1568172814563803997.json
